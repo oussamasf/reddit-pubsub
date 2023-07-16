0dc1b73577f4bf5a22c85d109644fbfb3e5aa746
Oussama Seffai <oussama.seffai@legal-doctrine.com>
2023-07-16 02:06:49 +0100

    add time to live limiter to protected routes

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PubSubController } from './pubsub/pubsub.controller';
 import { PubSubService } from './pubsub/pubsub.service';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
-  imports: [],
+  imports: [
+    ThrottlerModule.forRoot({
+      ttl: 60,
+      limit: 10,
+    }),
+  ],
   controllers: [AppController, PubSubController],
   providers: [AppService, PubSubService],
 })
--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,13 @@ import { Redis } from 'ioredis';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+
+  // primordial subscriber
   const sub = new Redis();
   sub.subscribe('my-channel-1', 'my-channel-2', (err, count) => {
     if (err) {
-      // Just like other commands, subscribe() can fail for some reasons,
-      // ex network issues.
       console.error('Failed to subscribe: %s', err.message);
     } else {
-      // `count` represents the number of channels this client are currently subscribed to.
       console.log(
         `Subscribed successfully! This client is currently subscribed to ${count} channels.`,
       );
